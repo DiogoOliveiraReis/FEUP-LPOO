9a8c76f56316e185f9d9fb9ba8e9a47cd9a6f67a
DoR18696 <36338524+DoR18696@users.noreply.github.com>
2018-04-20 15:21:35 +0100

    Initial commit

---
+++ Pkmn/desktop/src/com/dor/pkmn/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dor.pkmn.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dor.pkmn.Pkmn;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Pkmn(), config);
+	}
+}
